f149b4feebf0e061b086c64d97557fd77bc795e6
Fauzan <sangpemimpi24n@gmail.com>
2024-04-04 10:38:44 +0700

    fixing Git Ignore source

--- LPSTest.Core/obj/Debug/net7.0/LPSTest.Core.AssemblyInfo.cs
+++ LPSTest.Core/obj/Debug/net7.0/LPSTest.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LPSTest.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09180646b1473bc0632127a19f56f4e4be89fb96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed3646c9cca91d1f4b2ec6453530d8c0cdc0809e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LPSTest.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LPSTest.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
